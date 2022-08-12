9a3609b3feb5287b61627afb50baa6d3e56d3f4e
Paul Fleury <paul.fleuryp@gmail.com>
2022-08-12 08:19:13 +0200

    website backbone

--- app.py
+++ app.py
@@ -10,3 +10,24 @@ app = Dash(__name__)
 
 globe_trace_plot = px.scatter_geo()
 
+
+app.title = 'North Transatlantic 2022'
+
+app.layout = html.Div([
+    html.H1('North Transatlantic 2022'),
+
+    dcc.Tabs([
+        dcc.Tab(label='Globe', children=[
+            dcc.Graph(
+                id='transat_plot_globe',
+                figure=globe_trace_plot
+            )
+        ]),
+        dcc.Tab(label='Geo map'),
+        dcc.Tab(label='Sat map'),
+        dcc.Tab(label='Stats')
+    ]),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
