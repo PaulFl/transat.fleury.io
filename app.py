1f84f05e582ba7a62db8320de2fabf142dfc70ce
Paul Fleury <paul.fleuryp@gmail.com>
2022-08-12 21:29:21 +0300

    added plot & docker-compose.yaml

--- app.py
+++ app.py
@@ -7,9 +7,38 @@ app = Dash(__name__)
 
 # Loading pickles
 # notebook.fleury.io create pickle from final dataframe
+df_transat = pickle.load(open('pickles/dataframe_transat.pickle', 'rb'))  # type: pd.DataFrame
+df_transat_1h = pickle.load(open('pickles/dataframe_transat_1h.pickle', 'rb'))  # type: pd.DataFrame
 
-globe_trace_plot = px.scatter_geo()
-
+globe_trace_plot = px.scatter_geo(
+    df_transat_1h,
+    # title="Guadeloupe - France",
+    # height=1000,
+    lat='lat', lon='lon',
+    color='speed',
+    custom_data=['time'],
+    hover_data={'time': True, 'speed': ':.2f', 'lat': ':.2f', 'lon': ':.2f'},
+    labels={'lat': 'Lat', 'lon': 'Lon', 'speed': 'Speed', 'time': 'Time'},
+    projection="orthographic") \
+    .update_traces(
+    marker_size=3
+    ) \
+    .update_geos(
+    # resolution=50,
+    # fitbounds="locations",
+    projection_rotation_lon=-45,
+    projection_rotation_lat=30,
+    showcoastlines=True, coastlinecolor="RebeccaPurple",
+    showland=True, landcolor="LightGreen",
+    showocean=True, oceancolor="LightBlue",
+    showlakes=True, lakecolor="LightBlue",
+    showrivers=True, rivercolor="LightBlue",
+    lataxis_showgrid=True, lonaxis_showgrid=True,
+    lataxis_dtick=90, lonaxis_dtick=180
+    ) \
+    .update_layout(margin={"r": 0, "t": 50, "l": 0, "b": 10}) \
+ \
+    # .write_html('trace.html')
 
 app.title = 'North Transatlantic 2022'
 
@@ -20,7 +49,8 @@ app.layout = html.Div([
         dcc.Tab(label='Globe', children=[
             dcc.Graph(
                 id='transat_plot_globe',
-                figure=globe_trace_plot
+                figure=globe_trace_plot,
+                style={'width': '90vw', 'height': '80vh'}
             )
         ]),
         dcc.Tab(label='Geo map'),
